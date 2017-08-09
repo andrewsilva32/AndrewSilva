766b647fae37dd304ad2b5e6cef096342a3b7415
Lab. Desenvolvimento <Lab. Desenvolvimento@RB-LAB102-23>
2017-08-08 21:58:32 -0300

    Add project files.

---
+++ AndrewSilva/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using AndrewSilva.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AndrewSilva.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private static IList<Category> categoryList =
+            new List<Category>() {
+                new Category ( ) {categoryId=1, name = "keyboard"},
+                new Category() { categoryId = 2, name = "monitor"},
+                new Category() { categoryId = 3, name = "leptop"},
+                new Category() { categoryId = 4, name = "mouse"},
+                };
+        // GET: Categories
+        public ActionResult Index()
+        {
+            //select *
+            //from categories 
+            // order BY categories name
+            return View(categoryList.OrderBy(c => c.name));
+        }
+    }
+}
---
+++ AndrewSilva/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AndrewSilva.Models
+{
+    public class Category
+    {
+        public long categoryId { get; set; }
+        public string  name { get; set; }
+    }
+}
